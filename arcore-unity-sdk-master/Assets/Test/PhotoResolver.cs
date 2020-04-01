ade61fd7c576455aac58597886d07cdb8fba4a51
Aramis Real <dezesar@gmail.com>
2020-04-01 08:09:56 +0300

    Image Saving

--- arcore-unity-sdk-master/Assets/Test/PhotoResolver.cs
+++ arcore-unity-sdk-master/Assets/Test/PhotoResolver.cs
@@ -15,26 +15,29 @@ public class PhotoResolver : MonoBehaviour
     }
     IEnumerator UploadPNG()
     {
-        // We should only read the screen buffer after rendering is complete
-        yield return new WaitForEndOfFrame();
+        while (true)
+        {
+            // We should only read the screen buffer after rendering is complete
+            yield return new WaitForEndOfFrame();
 
-        // Create a texture the size of the screen, RGB24 format
-        int width = Screen.width;
-        int height = Screen.height;
-        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+            // Create a texture the size of the screen, RGB24 format
+            int width = Screen.width;
+            int height = Screen.height;
+            Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
 
-        // Read screen contents into the texture
-        /*tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        tex.Apply();*/
-        tex = (Texture2D)mat.mainTexture;
-        
-        byte[] bytes = tex.EncodeToPNG();
-        Object.Destroy(tex);
+            // Read screen contents into the texture
+            /*tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            tex.Apply();*/
+            tex = (Texture2D) mat.mainTexture;
 
-        // For testing purposes, also write to a file in the project folder
-        File.WriteAllBytes(Application.dataPath + "/SavedScreen.png", bytes);
-        Debug.Log("Shotted to: "+ Application.dataPath + " SavedScreen.png");
+            byte[] bytes = tex.EncodeToPNG();
+            Object.Destroy(tex);
 
+            // For testing purposes, also write to a file in the project folder
+            File.WriteAllBytes(Application.dataPath + "/SavedScreen.png", bytes);
+            Debug.Log("Shotted to: " + Application.dataPath + " SavedScreen.png" );
+            yield return new WaitForSeconds(1);
+        }
 
         // Create a Web Form
         /*WWWForm form = new WWWForm();
